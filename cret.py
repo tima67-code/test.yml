04d601ac1a0f05d172c4d25c4ed5570e70b5f68e
tima67-code <tima060608@gmail.com>
2026-04-02 15:04:15 +0500

    Create cret.py

---
+++ cret.py
@@ -0,0 +1,11 @@
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
